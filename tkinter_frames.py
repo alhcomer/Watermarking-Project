b68cb5a66fe65b580245a7c2f0c65f245c15a5fa
Aidan Comer <alhcomer@hotmail.co.uk>
2022-08-21 20:43:42 +0100

    added choose watermark window/watermark function

--- tkinter_frames.py
+++ tkinter_frames.py
@@ -4,17 +4,34 @@ from logging import root
 from operator import mod
 from tkinter.messagebox import YES
 from PIL import Image, ImageTk
-from tkinter import BOTH, BOTTOM, CENTER, LEFT, Button, PhotoImage, Tk, Toplevel, filedialog, Canvas
+from tkinter import BOTH, BOTTOM, CENTER, LEFT, Button, PhotoImage, Tk, Toplevel, filedialog, Canvas, Text
 from tkinter.ttk import Frame, Label
+import matplotlib.pyplot as plt
 
 
-def pop_up_window():
+def error_window():
     window = Toplevel()
     window.wm_title("Error")
     label = Label(window, text="Watermark Generator was unable to open that file. Ensure the file type is an image.")
     label.grid(row=0, column=0)
     button = Button(window, text='Okay', command=window.destroy)
     button.grid(row=1, column=0)
+    # TODO: should refactor above label widget making code to single function calls
+
+
+    # TODO: below function should be put into CheckImageFrame class
+def choose_watermark():
+    window = Toplevel()
+    label = Label(window, text="What text would you like to watermark the image with?")
+    label.grid(row=0, column=0)
+    label2 = Label(window, text="Text must be under 10 characters ")
+    label2.grid(row=1, column=0)
+    text_box = Text(window, height=1, width=20)
+    text_box.grid(row=2, column=0)
+    # TODO: bellow button needs command
+    button = Button(window, text="Ok.", command=None).grid(row=3, column=0)
+    
+
 
 class WaterMarkGenerator(Tk):
     def __init__(self):
@@ -54,7 +71,7 @@ class MainFrame(Frame):
                 self.tkinter_image = ImageTk.PhotoImage(self.image)
                 self.master.switch_frame(CheckImageFrame, image=self.tkinter_image)
             except IOError:
-                pop_up_window()
+                error_window()
 
 
 class CheckImageFrame(Frame):
@@ -69,11 +86,15 @@ class CheckImageFrame(Frame):
         # Need to add button commands
         #Use https://stackoverflow.com/questions/2261191/how-can-i-put-2-buttons-next-to-each-other to pack buttons
         Button(self, text="No", command=None).pack(side=BOTTOM)
-        Button(self, text="Yes", command=None).pack(side=BOTTOM)
+        Button(self, text="Yes", command=self._watermark_image).pack(side=BOTTOM)
         Label(self, text="Is this the image you would like to watermark?").pack(side=BOTTOM)
 
         # Need to refit all widgets so that they are in grid instead of pack
         # Geometry setting must happen at the end
+
+    def _watermark_image(self):
+        img = ImageTk.getimage(self.image)
+        img.show()        
 
 
 class CheckWaterMarkFrame(Frame):
