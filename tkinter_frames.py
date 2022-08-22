a34b61743b2d2f3d212ae0bd2e85abd12dc2c7ad
Aidan Comer <alhcomer@hotmail.co.uk>
2022-08-22 14:37:39 +0100

    added watermarking and image resizing

--- tkinter_frames.py
+++ tkinter_frames.py
@@ -3,11 +3,13 @@ from doctest import master
 from logging import root
 from operator import mod
 from tkinter.messagebox import YES
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw, ImageFont
 from tkinter import BOTH, BOTTOM, CENTER, LEFT, Button, PhotoImage, Tk, Toplevel, filedialog, Canvas, Text
 from tkinter.ttk import Frame, Label
 import matplotlib.pyplot as plt
+import pyautogui
 
+SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
 
 def error_window():
     window = Toplevel()
@@ -17,20 +19,6 @@ def error_window():
     button = Button(window, text='Okay', command=window.destroy)
     button.grid(row=1, column=0)
     # TODO: should refactor above label widget making code to single function calls
-
-
-    # TODO: below function should be put into CheckImageFrame class
-def choose_watermark():
-    window = Toplevel()
-    label = Label(window, text="What text would you like to watermark the image with?")
-    label.grid(row=0, column=0)
-    label2 = Label(window, text="Text must be under 10 characters ")
-    label2.grid(row=1, column=0)
-    text_box = Text(window, height=1, width=20)
-    text_box.grid(row=2, column=0)
-    # TODO: bellow button needs command
-    button = Button(window, text="Ok.", command=None).grid(row=3, column=0)
-    
 
 
 class WaterMarkGenerator(Tk):
@@ -73,10 +61,20 @@ class MainFrame(Frame):
             except IOError:
                 error_window()
 
+    def _resize_image(self):
+        image_width, image_height = self.image.size()
+        if image_width > (SCREEN_WIDTH - 50) or image_height > (SCREEN_HEIGHT - 50):
+            width_difference = image_width - SCREEN_WIDTH
+            height_difference = image_width - SCREEN_HEIGHT
+            pass
+        # TODO: need to resize image so that both image_width and image_height are 50 less than SCREEN_WIDTH and SCREEN_HEIGHT
+
+
 
 class CheckImageFrame(Frame):
     def __init__(self, master, image):
         Frame.__init__(self, master)
+        # TODO: scale down image size if greater than screen resolution
         self.image = image
         image_width = self.image.width()
         image_height = self.image.height()
@@ -86,15 +84,35 @@ class CheckImageFrame(Frame):
         # Need to add button commands
         #Use https://stackoverflow.com/questions/2261191/how-can-i-put-2-buttons-next-to-each-other to pack buttons
         Button(self, text="No", command=None).pack(side=BOTTOM)
-        Button(self, text="Yes", command=self._watermark_image).pack(side=BOTTOM)
+        Button(self, text="Yes", command=self._choose_watermark).pack(side=BOTTOM)
         Label(self, text="Is this the image you would like to watermark?").pack(side=BOTTOM)
 
         # Need to refit all widgets so that they are in grid instead of pack
         # Geometry setting must happen at the end
 
+    def _choose_watermark(self):
+        window = Toplevel()
+        label = Label(window, text="What text would you like to watermark the image with?")
+        label.grid(row=0, column=0)
+        label2 = Label(window, text="Text must be under 10 characters ")
+        label2.grid(row=1, column=0)
+        text_box = Text(window, height=1, width=20)
+        text_box.grid(row=2, column=0)
+        # TODO: need to set text limitations on text box
+        # TODO: add font choice drop down box
+        Button(window, text="Ok.", command=self._watermark_image).grid(row=3, column=0)
+        self.water_mark_text = text_box.get("1.0", "end")
+        window.destroy()
+
     def _watermark_image(self):
-        img = ImageTk.getimage(self.image)
-        img.show()        
+        watermark_image = ImageTk.getimage(self.image)
+        draw = ImageDraw.Draw(img_to_wm)
+        font = ImageFont.truetype("arial.ttf", 50)
+        draw.text((0, 0), self.water_mark_text, (0, 0, 0), font=font)
+        plt.subplot(1, 2, 1)
+        plt.title("black text")
+        plt.imshow(watermark_image)
+        # img.show()
 
 
 class CheckWaterMarkFrame(Frame):
