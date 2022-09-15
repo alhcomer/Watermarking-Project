0de90ff1104eac1a5e841cf60c59812810935295
Aidan Comer <alhcomer@hotmail.co.uk>
2022-09-15 20:51:58 +0100

    centered initial window

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from tkinter_frames import WaterMarkGenerator
+from tkinter import Menu, Frame, Label, Toplevel
+from tkinter_frames import SCREEN_WIDTH, SCREEN_HEIGHT
+
+
+def center(window):
+    window.update_idletasks()
+    size = tuple(int(_) for _ in window.geometry().split('+')[0].split('x'))
+    x = SCREEN_WIDTH/2 - size[0]/2
+    y = SCREEN_HEIGHT/2 - size[1]/2 
+    window.geometry("+%d+%d" % (x, y))
+
 
 def main():
     app = WaterMarkGenerator()
+    center(app)
+    
     app.mainloop()
 
 if __name__ == '__main__':
